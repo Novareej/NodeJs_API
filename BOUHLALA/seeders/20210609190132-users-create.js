9babf7e06cf3ee387fccb07bb4144077fa8dfce4
Arij Bouhlala <bouhlalaarij@gmail.com>
2021-06-10 18:23:20 +0100

    second commit

---
+++ BOUHLALA/seeders/20210609190132-users-create.js
@@ -0,0 +1,45 @@
+'use strict';
+const { random } = require('faker');
+const faker = require('faker');
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+   
+     const usersData = [];
+     const roles = ['admin','author','guest']
+    
+     for (let i = 0; i < 20; i++) {
+         const date = faker.date.past()
+         const user = {
+             username: faker.internet.userName(),
+             email: faker.internet.email(),
+             password:faker.internet.password(),
+             role:faker.helpers.randomize(roles),
+             createdAt:date,
+             updatedAt: date
+         }
+         usersData.push(user)
+        }
+        const objs = queryInterface.bulkInsert('Users', usersData);
+        console.log(objs);
+         return objs;
+
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
