f2e714bf02cc310901edd563a7a1ea606d4dbb8b
Arij Bouhlala <bouhlalaarij@gmail.com>
2021-06-12 16:47:13 +0100

    commit

--- BOUHLALA/routes/users.js
+++ BOUHLALA/routes/users.js
@@ -1,9 +1,29 @@
-var express = require('express');
-var router = express.Router();
+const router = require('express').Router();
+ const usersRepo = require('../repositories/users')
+ /* GET users listing. */
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+ router.get('/', async function(req, res, next) {
+   res.send(await usersRepo.getUsers(offset,limit))
+ });
+
+ router.get('/:id', async function(req, res, next) {
+  res.send(await usersRepo.getUsers(req.params.id))
 });
+ module.exports = router;
+ 
+ router.post('/',async function(req,res,next){
+  const { role } = req.user;
+  if(role == "admin" || role == "author"){
+    const user = req.body
+    const retrievedUser = await usersRepo.getUserByEmail(user.email) 
+    if(!retrievedUser){
+      res.send(await usersRepo.addUser(user))
+    }else{
+      res.status(400).json({ message: 'Email already exists!' })
+    }
+  }else{
+    res.status(403).json({ message: 'unauthorised access!' })
+  }
+})
 
-module.exports = router;
